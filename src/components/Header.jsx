9f07db44d034c50f4bb0403e3532539762516925
Jader Cano <jader.cano@contractors.accruent.com>
2021-05-22 07:44:58 -0500

    Added Header and Character components. Added useState to set state to the components, and added useEffect to get characters from an API

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+const Header = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const handleClick = () => {
+    setDarkMode(!darkMode);
+  };
+
+  return (
+    <div className="Header">
+      <h1>React Hooks</h1>
+      <button type="button" onClick={handleClick}>
+        {darkMode ? "Light Mode" : "Dark Mode"}
+      </button>
+    </div>
+  );
+};
+
+export default Header;
